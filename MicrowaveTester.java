0102dfea6987a77474adc2e502c00b4a8650fc79
XabienL <98927144+XabienL@users.noreply.github.com>
2023-02-12 13:55:03 -0500

    Microwave Tester

---
+++ MicrowaveTester.java
@@ -0,0 +1,16 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		myMicrowave myMicrowave = new myMicrowave();
+		myMicrowave.time();
+		myMicrowave.time();
+		myMicrowave.power();
+		myMicrowave.start();
+		myMicrowave.reset();
+		
+		
+
+	}
+
+}
